78f58bee1aa15cff900d026aa4976652147092ca
Rajarajan Abathsagayam <rajarajan.abathsagayam@gmail.com>
2025-02-07 13:42:55 +0530

    cleared boilerplate code

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import styles from "./index.style";
+
+function index() {
+  return (
+    <View style={styles.container}>
+      <Text>Test</Text>
+    </View>
+  );
+}
+export default index;
